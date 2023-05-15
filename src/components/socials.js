02f42a013e351137b37289ec6c5357a059e8617f
Stranger Mushwana <strangermushwana@gmail.com>
2023-05-15 16:06:41 +0200

    setup for animation

---
+++ src/components/socials.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Socials() {
+  return (
+    <div className="bg-white">
+      <Image src="/images/facebook.png" width={25} height={25} alt="Facebook" />
+    </div>
+  )
+}
