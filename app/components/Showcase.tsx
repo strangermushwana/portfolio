14a36efd3b3e78d0f5edf6f43722753ec929a48a
Stranger Mushwana <strangermushwana@gmail.com>
2023-06-21 15:15:37 +0200

    fixed showcase

--- app/components/Showcase.tsx
+++ app/components/Showcase.tsx
@@ -28,7 +28,7 @@ export default function Showcase() {
               passion for technology. I enjoy problem-solving and constantly
               seek new ways to improve. 🚀
             </div>
-            <div className=" flex space-x-4 items-center my-8">
+            <div className="flex space-x-4 items-center mt-8">
               <a
                 href="#"
                 className="px-8 py-3 bg-green-900 hover:bg-green-800 text-white rounded-md font-semibold text-xs"
@@ -41,6 +41,36 @@ export default function Showcase() {
               >
                 Download Resume
               </a>
+            </div>
+            <div className="flex space-x-3 items-center mt-6">
+              <div className="p-2 text-center overflow-hidden rounded-full bg-white cursor-pointer">
+                <Image
+                  src="/images/github.png"
+                  alt="Image of myself"
+                  width={20}
+                  height={20}
+                />
+              </div>
+              <div className="rounded-full bg-white cursor-pointer">
+                <div className="p-2 text-center overflow-hidden rounded-full bg-white">
+                  <Image
+                    src="/images/linkedin.png"
+                    alt="Image of myself"
+                    width={20}
+                    height={20}
+                  />
+                </div>
+              </div>
+              <div className="rounded-full bg-white cursor-pointer">
+                <div className="p-2 text-center overflow-hidden rounded-full bg-white">
+                  <Image
+                    src="/images/twitter.png"
+                    alt="Image of myself"
+                    width={20}
+                    height={20}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -61,12 +91,44 @@ export default function Showcase() {
                 height={100}
               />
             </div>
+            <div className="absolute overflow-hidden z-100 top-10 left-0">
+              <Image
+                alt="Nodejs Logo"
+                src="/images/docker.png"
+                width={90}
+                height={90}
+              />
+            </div>
+            <div className="absolute overflow-hidden z-100 top-[19rem] left-80">
+              <Image
+                alt="Nodejs Logo"
+                src="/images/typescript.png"
+                width={120}
+                height={120}
+              />
+            </div>
             <div className="absolute bg-white overflow-hidden z-100 top-32 left-64">
               <Image
                 alt="Angular Logo"
                 src="/images/anglar.png"
                 width={100}
                 height={100}
+              />
+            </div>
+            <div className="absolute overflow-hidden z-100 top-[10rem] right-3">
+              <Image
+                alt="React Logo"
+                src="/images/visual-studio.png"
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="absolute overflow-hidden z-100 top-[10rem] right-32">
+              <Image
+                alt="React Logo"
+                src="/images/postgre.png"
+                width={50}
+                height={50}
               />
             </div>
             <div className="absolute overflow-hidden z-100 top-[18rem] right-3">
