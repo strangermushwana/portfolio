30ec2759cf15d09a0a1b739aa6a65a12c4c3b0dc
Stranger Mushwana <strangermushwana@gmail.com>
2023-06-22 14:10:15 +0200

    work history

--- app/components/WorkHistory.tsx
+++ app/components/WorkHistory.tsx
@@ -1,52 +1,55 @@
 import Image from "next/image"
 import React from "react"
 
-export default function WorkHistory() {
+export interface IEduction {
+  history: {
+    jobTitle: string
+    company: string
+    image: string
+    description: string
+    languages: string[]
+    term: string
+  }
+}
+
+export default function WorkHistory({ history }: IEduction) {
+  const colors = [
+    "bg-red-200",
+    "bg-yellow-500",
+    "bg-green-800",
+    "bg-red-800",
+    "bg-blue-800",
+    "bg-orange-700",
+    "bg-purple-800",
+  ]
   return (
     <div className="py-6 bg-pl-100 mx-auto max-w-6xl">
       <div className="flex flex-col items-center space-x-10">
         <div className="w-full">
           <div className="flex items-center shadow-md py-10 px-5 w-full">
             <div className="flex flex-col pl-4">
-              <Image
-                src="/images/jobjack.png"
-                alt=""
-                width={350}
-                height={400}
-              />
+              <Image src={history.image} alt="" width={350} height={400} />
               <div className="font-semibold text-2xl mt-4 text-yellow-600">
-                Software Engineer
+                {history.jobTitle}
               </div>
               <div className="text-md font-thin my-1 uppercase">
-                May 2021 - Current
+                {history.term}
               </div>
               <div className="font-sm my-1 max-w-4xl">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non
-                temporibus modi sequi in eum pariatur at iure aliquid, numquam
-                quasi fugiat excepturi, deleniti, eos neque recusandae dolor
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse
-                placeat enim optio obcaecati praesentium et nesciunt ad, laborum
-                dicta eligendi? magni
+                {history.description}
               </div>
               <div className="grid gap-y-3 gap-x-2 grid-cols-4 w-fit py-8 font-mono">
-                <div className="bg-red-200 text-xs w-fit rounded-full font-bold text-center px-6 py-2 text-white">
-                  Angular
-                </div>
-                <div className="bg-yellow-500 text-xs w-fit rounded-full font-bold text-center px-6 py-2 text-white">
-                  Node.js
-                </div>
-                <div className="bg-green-800 text-xs w-fit rounded-full font-bold text-center px-6 py-2 text-white">
-                  MongoDB
-                </div>
-                <div className="bg-red-800 text-xs w-fit rounded-full font-bold text-center px-6 py-2 text-white">
-                  Nest.js
-                </div>
-                <div className="bg-blue-800 text-xs w-fit rounded-full font-bold text-center px-6 py-2 text-white">
-                  Postgres
-                </div>
-                <div className="bg-purple-800 text-xs w-fit rounded-full font-bold text-center px-6 py-2 text-white">
-                  GraphQL
-                </div>
+                {history.languages.map((language, index) => (
+                  <div
+                    key={language}
+                    className={
+                      colors[index] +
+                      " text-xs w-fit rounded-full font-bold text-center px-6 py-2 text-white"
+                    }
+                  >
+                    {language}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,36 @@ import Showcase from "./components/Showcase"
 import WorkHistory from "./components/WorkHistory"
 
 export default function Home() {
+  const workHistory = [
+    {
+      id: 1,
+      jobTitle: "Software Developer",
+      company: "JOBJACK",
+      term: "May 2021 - Current",
+      image: "/images/jobjack.png",
+      description:
+        "As a Developer, I played a pivotal role in leading and contributing to the successful delivery of complex software projects. Leveraging my extensive technical expertise, I spearheaded the design, development, and implementation of innovative solutions while ensuring adherence to best practices and industry standards.",
+      languages: [
+        "Angular",
+        "Node.js",
+        "Postgres",
+        "TypeScript",
+        "MongoDB",
+        "GraphQL",
+        "Nest.js",
+      ],
+    },
+    {
+      id: 2,
+      jobTitle: "Software Engineer Intern",
+      company: "ORACLE",
+      term: "March 2020 - March 2021",
+      image: "/images/oracle.png",
+      description:
+        "I had the opportunity to gain valuable hands-on experience and contribute to various software development projects. Throughout my internship, I worked closely with the development team, assisting in the design, implementation, and testing of software solutions.",
+      languages: ["Java", "HTML & CSS", "JavaScript", "XML", "PL/SQL", "REST"],
+    },
+  ]
   return (
     <main className="h-screen text-gray-700">
       <Showcase />
@@ -12,12 +42,11 @@ export default function Home() {
         </div>
       </div>
       <div className="flex flex-col items-center">
-        <div className="w-full">
-          <WorkHistory />
-        </div>
-        <div className="w-full">
-          <WorkHistory />
-        </div>
+        {workHistory.map((history) => (
+          <div key={history.id} className="w-full">
+            <WorkHistory history={history} />
+          </div>
+        ))}
       </div>
       <div className="bg-black p-10">
         <div className="max-w-6xl mx-auto text-white">Hello</div>
